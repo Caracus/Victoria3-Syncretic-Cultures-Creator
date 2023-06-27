9389ffb6813ab653d3137f6b21f824b9d6cd5433
Caracus <sascha.hans@web.de>
2023-06-27 11:51:50 +0200

    adjust config json and korean name

--- src/main/kotlin/org/victoria_3_syncretic_cultures_creator/logic/LogicUtils.kt
+++ src/main/kotlin/org/victoria_3_syncretic_cultures_creator/logic/LogicUtils.kt
@@ -3,6 +3,31 @@ package org.victoria_3_syncretic_cultures_creator.logic
 import org.victoria_3_syncretic_cultures_creator.models.SyncreticCulture
 
 fun createMutuallyExclusiveCulturesMap(syncreticCultureList: List<SyncreticCulture>): List<SyncreticCulture> {
+    val cultureToSyncreticCultureMap: MutableMap<String, MutableSet<String>> = mutableMapOf()
+    val syncreticCultureMutuallyExclusiveMap: MutableMap<String, MutableSet<String>> = mutableMapOf()
+
+    for (syncreticCulture in syncreticCultureList) {
+        for (baseCulture in syncreticCulture.baseCultures) {
+            val existingSet = cultureToSyncreticCultureMap.getOrPut(baseCulture) { mutableSetOf() }
+            existingSet += syncreticCulture.syncreticCultureName
+        }
+        syncreticCultureMutuallyExclusiveMap[syncreticCulture.syncreticCultureName] = syncreticCulture.mutuallyExclusiveWith.toMutableSet()
+    }
+
+    for ((baseCulture, syncreticCultures) in cultureToSyncreticCultureMap) {
+        val uniqueSyncreticCultures = syncreticCultures.toSet()
+        for (syncreticCultureName in syncreticCultures) {
+            val mutuallyExclusiveWith = syncreticCultureMutuallyExclusiveMap[syncreticCultureName]!!
+            mutuallyExclusiveWith += uniqueSyncreticCultures - syncreticCultureName
+        }
+    }
+
+    return syncreticCultureList.map { syncreticCulture ->
+        syncreticCulture.copy(mutuallyExclusiveWith = syncreticCultureMutuallyExclusiveMap[syncreticCulture.syncreticCultureName]!!)
+    }
+}
+
+fun createMutuallyExclusiveCulturesMapOld(syncreticCultureList: List<SyncreticCulture>): List<SyncreticCulture> {
     // create a map that represents which syncretic cultures are using the culture in question
     var cultureToSyncreticCultureMap: MutableMap<String, Set<String>> = mutableMapOf()
 
